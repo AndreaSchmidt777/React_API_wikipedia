3bba6f68b6558b99fa754bc8c5de959493fbbeda
andsmt <andreaschmidt.bayreuth@gmail.com>
2020-03-29 22:23:03 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React is cool
+          Learn React is cool!
         </a>
       </header>
     </div>
