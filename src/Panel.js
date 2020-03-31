e182c8d05a238b342c37be709eeca5d01780ed61
andsmt <andreaschmidt.bayreuth@gmail.com>
2020-03-31 10:54:51 +0200

    Projektvorbereitungen

---
+++ src/Panel.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./Panel.css";
+
+class Panel extends Component {
+
+    render() {
+        return(
+            <div className="Panel">
+                <h4 className="Panel-title">{this.props.title}</h4>
+                <div className="Panel-content">{this.props.content}</div>
+            </div>
+        )
+    }
+}
+
+export default Panel;
