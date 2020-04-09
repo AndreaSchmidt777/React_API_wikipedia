c64d8ba3b30170f87ce4ff5d86c37fadf3a2f9e6
andsmt <andreaschmidt.bayreuth@gmail.com>
2020-04-09 22:57:14 +0200

    API Mediawiki

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import superagent from 'superagent';
+import jsonp from 'superagent-jsonp';
 
 export function incrementCounter() {
     return {type: "INCREMENT"};
@@ -8,5 +10,17 @@ export function addTodo(title) {
 }
 
 export function wikipediaSearch(keyword) {
-    return {type: "WIKIPEDIA_SEARCH", keyword: keyword};
+    return function(dispatch) {
+        let url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + keyword + "&format=json&callback=JSONP_CALLBACK";
+        superagent
+            .get(url)
+            .use(jsonp({
+                callbackName: 'JSONP_CALLBACK'
+            }))
+            .end((err, res) => {
+                dispatch({type: "WIKIPEDIA_SEARCH", keyword: keyword, res: res})
+            })
+    
+        
+    }
 }
