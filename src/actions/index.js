a6881bb9e684d9672ff21acf47f3da59600c52fc
andsmt <andreaschmidt.bayreuth@gmail.com>
2020-04-09 23:16:54 +0200

    Wiederherstellung altes  Projekt

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,3 @@
-import superagent from 'superagent';
-import jsonp from 'superagent-jsonp';
 
 export function incrementCounter() {
     return {type: "INCREMENT"};
@@ -10,17 +8,5 @@ export function addTodo(title) {
 }
 
 export function wikipediaSearch(keyword) {
-    return function(dispatch) {
-        let url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + keyword + "&format=json&callback=JSONP_CALLBACK";
-        superagent
-            .get(url)
-            .use(jsonp({
-                callbackName: 'JSONP_CALLBACK'
-            }))
-            .end((err, res) => {
-                dispatch({type: "WIKIPEDIA_SEARCH", keyword: keyword, res: res})
-            })
-    
-        
-    }
+    return {type: "WIKIPEDIA_SEARCH", keyword: keyword};
 }
--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import Wikipedia from './components/Wikipedia';
 import Navigation from './components/Navigation';
 import WikipediaResults from './components/WikipediaResults';
 import Home from './components/Home';
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux';
 import { incrementCounter } from './actions/index';
 import counter from './reducers/index';
@@ -15,7 +14,7 @@ import './index.css';
 
 import { HashRouter, Route } from 'react-router-dom';
 
-let store = createStore(counter, applyMiddleware(thunkMiddleware));
+let store = createStore(counter);
 console.log(store.getState());
 
 ReactDOM.render(
