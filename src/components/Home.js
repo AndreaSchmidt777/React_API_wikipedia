6f6d63ae41f431a5cc916adfc6d04fa98c06ba1c
andsmt <andreaschmidt.bayreuth@gmail.com>
2022-08-28 14:50:03 +0200

    TEst

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import Panel from './Panel';
 class Home extends Component {
     render() {
         return (
-            <Panel title="Startseite Andrea">
+            <Panel title="Startseite">
                 <h1>Home - Seite!</h1>
             </Panel>
         )
